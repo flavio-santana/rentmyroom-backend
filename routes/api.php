9c6894abb4529de9162ac194bcff3e27f662757f
Programador <programador@haskell.com.br>
2020-09-17 08:16:35 -0300

    adicionado comentários nos serviços

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/anuncios','AnuncioController');
 
+// Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/comodidades','ComodidadeController');
 
+// Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/imoveis','ImovelController');
 
+// Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/regras','RegraController');
 
+// Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/tiposimoveis','TipoImovelController');
 
+// Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/tiposquartos','TipoQuartoController');
 
+// Aqui, temos encapsulado os métodos index(get), store(post), show(get), update(put) e destroy(delete)
 Route::apiResource('/usuarios','UsuarioController');
