69820325e02282f36715f6b43a7606300943d102
Flavio Santana <f.o.santana@gmail.com>
2020-09-26 09:27:59 -0300

    correções implementadas em model/user.php email unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
+/**
+ * No link abaixo encontrei a solução para o erro que 
+ * estava ocorrendo para: $table->string('email')->unique(); 
+ */
+//https://laravel-news.com/laravel-5-4-key-too-long-error
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -14,12 +21,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::connection('mysql_usuario')->create('users', function (Blueprint $table) {
+            
             $table->id();
+            
             $table->string('name');
+            
             $table->string('email')->unique();
+            
             $table->timestamp('email_verified_at')->nullable();
+            
             $table->string('password');
+            
             $table->rememberToken();
+
             $table->timestamps();
         });
     }
--- database/migrations/2020_09_23_111727_create_sessions_table.php
+++ database/migrations/2020_09_23_111727_create_sessions_table.php
@@ -14,7 +14,7 @@ class CreateSessionsTable extends Migration
     public function up()
     {
         Schema::connection('mysql_usuario')->create('sessions', function (Blueprint $table) {
-            $table->string('id')->unique();
+            $table->increments('id');
             $table->foreignId('user_id')->nullable();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
